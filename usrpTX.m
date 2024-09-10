d8e89001f9e827c65d4221e33daf2bccaaffea81
ataobaby <87025102+ataobaby@users.noreply.github.com>
2024-09-10 09:58:06 +0800

    Add files via upload

---
+++ usrpTX.m
@@ -0,0 +1,38 @@
+clear,clc;
+
+%% 配置参数
+SF = 10;  %扩频因子
+BW = 500e3; %带宽
+fc = 915e6; %发射频段
+Power = 14; %功率
+message = "1" ;
+Fs = 5e6 ;
+tx = sdrtx('Pluto','RadioID','usb:0','CenterFrequency',fc,...
+    'BasebandSampleRate',Fs,...
+    'SamplesPerFrame',2e6,'Gain',0);
+rx = sdrrx('Pluto','RadioID','usb:0','CenterFrequency',fc,...
+    'BasebandSampleRate',Fs,...
+    'OutputDataType','double',...
+    'SamplesPerFrame',2e6,'Gain',60);    
+%% Transmit Signal
+signalIQ = LoRa_Tx(message,BW,SF,Power,Fs,0);
+upChirp = signalIQ(1:10240);
+Tdata = signalIQ;
+Tdata=[zeros(length(Tdata),1);Tdata];
+
+%% 发射流程
+while 1
+    tx(Tdata);
+    disp('ping');
+    k =  0;
+    while 1
+        k = k+1;
+        Rdata = rx();
+        signalIndex = signalCapture(Rdata,upChirp,8,0);
+        if signalIndex ~= -1 || k == 1
+           break;
+        end
+    end
+end
+release(tx);
+release(rx);
