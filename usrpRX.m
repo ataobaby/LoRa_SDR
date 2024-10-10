95c233c4f65f808f208c707008e388e2e4a8ec36
ataobaby <87025102+ataobaby@users.noreply.github.com>
2024-10-10 13:44:21 +0800

    Add files via upload

--- usrpRX.m
+++ usrpRX.m
@@ -3,37 +3,50 @@ clear,clc;
 %% 配置参数
 SF = 10;  %扩频因子
 BW = 500e3; %带宽
-fc = 915e6; %发射频段
+fc = 433e6; %发射频段
 Power = 14; %功率
-message = "1" ;
-Fs = 5e6 ;
+
+Fs = 1e6 ;
 tx = sdrtx('Pluto','RadioID','usb:0','CenterFrequency',fc,...
     'BasebandSampleRate',Fs,...
-    'SamplesPerFrame',2e6,'Gain',0);
+    'Gain',0);
 rx = sdrrx('Pluto','RadioID','usb:0','CenterFrequency',fc,...
     'BasebandSampleRate',Fs,...
     'OutputDataType','double',...
+    'GainSource','AGC Fast Attack', ...
     'SamplesPerFrame',2e6,'Gain',60);    
 %% Transmit Signal
-signalIQ = LoRa_Tx(message,BW,SF,Power,Fs,0);
-upChirp = signalIQ(1:10240);
-Tdata = signalIQ;
-Tdata=[zeros(length(Tdata),1);Tdata];
-
+for num = 1:10
+    signalIQ = LoRa_Tx(char(num+96),BW,SF,Power,Fs,0); 
+    Tdata(:,num) =[zeros(length(signalIQ ),1);signalIQ ];
+end
+upChirp = conj(signalIQ(1:10240/5));
 %% 发射流程
+SendIndex = 1;
+baseBandData = [];
 while 1
     k =  0;
     while 1
         k = k+1;
         Rdata = rx();
+%         figure(1)
+%         plot(abs(Rdata));
+%         spectrogram(Rdata,300,0,500,Fs,'yaxis','centered');
         signalIndex = signalCapture(Rdata,upChirp,8,0);
-        if signalIndex ~= -1 
-           disp(char(LoRa_Rx(Rdata(signalIndex:min([signalIndex+309759,2e6])),BW,SF,2,Fs,0))) ;
-           break;
+        if signalIndex ~= -1 && signalIndex+61951 <= 4e6
+            numStr = LoRa_Rx(conj(Rdata(signalIndex:min([signalIndex+61951,4e6]))),BW,SF,2,Fs,0);
+            if numStr>47 && numStr<58
+                disp('ping')
+                disp(char(numStr));
+                Data = [numStr;Rdata(signalIndex:min([signalIndex+61951,4e6]))];
+                SendIndex = numStr - 47;
+                baseBandData = [baseBandData,Data];
+%                 release(rx);
+                break;
+            end
         end
     end
     disp('pong');
-    tx(Tdata);
+    tx(Tdata(:,SendIndex));
+%     release(tx);
 end
-release(tx);
-release(rx);
