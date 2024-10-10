571ccea67015f50a9decfe4e7e207b6b69443f97
ataobaby <87025102+ataobaby@users.noreply.github.com>
2024-10-10 13:42:37 +0800

    Add files via upload

--- usrpTX.m
+++ usrpTX.m
@@ -3,36 +3,51 @@ clear,clc;
 %% 配置参数
 SF = 10;  %扩频因子
 BW = 500e3; %带宽
-fc = 915e6; %发射频段
+fc = 433e6; %发射频段
 Power = 14; %功率
-message = "1" ;
-Fs = 5e6 ;
+
+Fs = 1e6 ;
 tx = sdrtx('Pluto','RadioID','usb:0','CenterFrequency',fc,...
     'BasebandSampleRate',Fs,...
-    'SamplesPerFrame',2e6,'Gain',0);
+    'Gain',0);
 rx = sdrrx('Pluto','RadioID','usb:0','CenterFrequency',fc,...
     'BasebandSampleRate',Fs,...
     'OutputDataType','double',...
-    'SamplesPerFrame',2e6,'Gain',60);    
-%% Transmit Signal
-signalIQ = LoRa_Tx(message,BW,SF,Power,Fs,0);
-upChirp = signalIQ(1:10240);
-Tdata = signalIQ;
-Tdata=[zeros(length(Tdata),1);Tdata];
+    'GainSource','AGC Fast Attack',...
+    'SamplesPerFrame',4e6,'Gain',60);  
 
+%% Transmit Signal
+for num = 1:10
+    signalIQ = LoRa_Tx(char(num+47),BW,SF,Power,Fs,0); 
+    signalIQ = conj(signalIQ);
+    Tdata(:,num) =[zeros(ceil(length(signalIQ)*0.1),1);signalIQ;zeros(length(signalIQ)*1,1)];
+end
+upChirp = conj(signalIQ(1:10240/5));
 %% 发射流程
+SendIndex = 1;
+baseBandData = [];
 while 1
-    tx(Tdata);
+    tx(Tdata(:,SendIndex));
     disp('ping');
     k =  0;
     while 1
         k = k+1;
         Rdata = rx();
-        signalIndex = signalCapture(Rdata,upChirp,8,0);
-        if signalIndex ~= -1 || k == 1
-           break;
+        signalIndex = signalCapture(Rdata,upChirp,10,0);
+        if signalIndex ~= -1 && signalIndex + 61951 <= 4e6
+            numStr = LoRa_Rx(Rdata(signalIndex:signalIndex+61951),BW,SF,2,Fs,0);
+            disp('pong');
+            if numStr>96 && numStr <107
+                disp(char(numStr));
+                Data = [numStr;Rdata(signalIndex:signalIndex+61951)];
+                baseBandData = [baseBandData,Data];
+                if numStr == 96 + SendIndex
+                    SendIndex = mod(numStr-96, 10)+1;
+                    break;
+                end
+            end
+        elseif k >= 2
+            break;
         end
     end
 end
-release(tx);
-release(rx);
